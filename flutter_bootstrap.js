5a2c7554ad0ef1e014918a5e8f5969b580652ae1
sisopop <sisopop@users.noreply.github.com>
2026-07-04 16:01:56 +0000

    deploy: 7bf3a758e7841b4af5fe549cb466d13c98259886

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3720849417" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2676654909" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
